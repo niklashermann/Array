8da353a35fc7e5c56e90a42e021d9e3519b877a1
edu <edu@PC>
2023-01-17 18:05:08 +0100

    Projektdateien hinzufügen.

---
+++ ConsoleApp3/Auto.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayÜbungen
+{
+    public class Auto
+    {
+        // Felder (Fields)
+        int raeder;
+        double hoechstgeschwindigkeit;
+        //double aktuelleGeschwindigkeit;
+
+
+        // Eigenschaften (Properties)
+        public int Raeder
+        {
+            get { return raeder; }
+        }
+
+        //public double AktuelleGeschwindigkeit
+        //{
+        //    set { aktuelleGeschwindigkeit = value; }
+        //    get { return aktuelleGeschwindigkeit; }
+        //}
+
+        public double AktuelleGeschwindigkeit
+        {
+            set;
+            get;
+        }
+
+        // Konstruktor
+        public Auto()
+        {
+            raeder = 4;
+            hoechstgeschwindigkeit = 100;
+            AktuelleGeschwindigkeit = 0;
+        }
+
+
+        // Methoden (Methods)
+        //public void SetReader(int raeder)
+        //{
+        //    this.raeder = raeder;
+        //}
+
+        //public int GetReader()
+        //{
+        //    return this.raeder;
+        //}
+
+        public void Beschleunigen(double wert)
+        {
+            AktuelleGeschwindigkeit += wert;
+            Console.WriteLine($"Auto beschleunigt um {wert} Km/h. Auto fährt jetzt {AktuelleGeschwindigkeit} Km/h");
+            //aktuelleGeschwindigkeit = aktuelleGeschwindigkeit + wert;
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,51 @@
+﻿//using ArrayÜbungen;
+
+//Auto auto1 = new Auto();
+//Auto auto2 = new Auto();
+//auto1.Raeder = 10;
+//auto2.Raeder = 13;
+
+//auto1.SetReader(11);
+//auto2.SetReader(13);
+
+//int raeder = auto1.Raeder;
+
+//auto1.Beschleunigen(5);
+//auto1.Beschleunigen(10);
+//auto1.Beschleunigen(5);
+//auto1.Beschleunigen(20);
+
+
+int zeilen = 21;
+int[] zahl = new int[zeilen];
+var random = new Random();
+for (int i = 1; i < zeilen; i++)
+{
+    zahl[i] = random.Next(1002);
+}
+
+int ergebnis = MaxWert(zahl);
+    
+Console.WriteLine("Die größte Zahl ist {0}", ergebnis);
+
+// Programmende
+
+/// Funktionsbereich
+
+static int MaxWert(int[] zahl)
+{
+
+     int speicher = 0;
+    for (int i = 1; i < 21; i++)
+    {
+        Console.WriteLine("Zahl {0}: {1}", i, zahl[i]);
+        if (zahl[i] > speicher )
+        {
+            speicher = zahl[i];
+        }
+     
+    }
+    int ergebnis = speicher;
+    return ergebnis;
+}
+
